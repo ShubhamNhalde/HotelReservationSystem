a175f5adae51d49bfc2e7aa7cee822ef030c78d6
Shubham <nhaldes@gmail.com>
2022-03-07 08:22:23 +0530

    Added: Welcom to hotel reservation system program

---
+++ WelcomPage.java
@@ -0,0 +1,8 @@
+package www.bridgelabz.HotelReservationSystem;
+
+public class WelcomPage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation system Program ");
+	}
+
+}
